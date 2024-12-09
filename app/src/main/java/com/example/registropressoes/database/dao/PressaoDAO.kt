c6599045776ca43d569d3a23ec244cc45fc06234
Inguim <igorazevedost@gmail.com>
2024-12-09 17:12:16 -0300

    feat: adicionado querys de maxima, minima e AVG para indicadores

--- app/src/main/java/com/example/registropressoes/database/dao/PressaoDAO.kt
+++ app/src/main/java/com/example/registropressoes/database/dao/PressaoDAO.kt
@@ -9,6 +9,11 @@ import androidx.room.Update
 import com.example.registropressoes.model.Pressao
 import kotlinx.coroutines.flow.Flow
 
+data class MediaPressao (
+    val avg_maxima: Double,
+    val avg_minima: Double
+)
+
 @Dao
 interface PressaoDAO {
     @Query("SELECT * FROM Pressao WHERE data BETWEEN :dataStart AND :dataEnd ORDER BY data DESC")
@@ -31,4 +36,14 @@ interface PressaoDAO {
 
     @Query("SELECT * FROM Pressao ORDER BY data DESC LIMIT 1")
     suspend fun listarUltimaMedicao(): List<Pressao>
+
+    @Query("SELECT * FROM Pressao ORDER BY maxima ASC LIMIT 1")
+    suspend fun listarMaiorMedicao(): Pressao?
+
+    @Query("SELECT * FROM Pressao ORDER BY minima ASC LIMIT 1")
+    suspend fun listarMenorMedicao(): Pressao?
+
+    @Query("SELECT AVG(maxima) as avg_maxima, AVG(minima) as avg_minima FROM Pressao")
+    suspend fun listarMediaMedicoes(): MediaPressao
+
 }
